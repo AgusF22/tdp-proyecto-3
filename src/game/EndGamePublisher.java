222181e76daa548c1a7c3a2b93a331b67c719448
agustin islas <agustinislas44@hotmail.com>
2021-10-30 11:44:44 -0300

    Implementación parcial clases EndGamePublisher, Enemy, EnemyState y Player

--- src/game/EndGamePublisher.java
+++ src/game/EndGamePublisher.java
@@ -1,10 +1,11 @@
 package game;
 
+import game.entity.player.Player;
 
 public class EndGamePublisher {
 	//TODO documentar
 	
-	private EndGamePublisher instance;
+	private static EndGamePublisher instance;
 	protected Subscriber[] subscribers; //TODO definir coleccion
 	
 	private EndGamePublisher() {
@@ -15,13 +16,15 @@ public class EndGamePublisher {
 	 * Devuelve la instancia actual del objeto, o en su defecto un nuevo objeto.
 	 * @return
 	 */
-	public EndGamePublisher getInstance() {
-		//TODO imp
-		return null;
+	public static EndGamePublisher getInstance() {
+		if(instance == null) 
+			instance = new EndGamePublisher();
+		return instance;
 	}
 	
 	public void subscribe(Subscriber s) {
 		//TODO implementar
+		
 	}
 	
 	public void unsubscribe(Subscriber s) {
