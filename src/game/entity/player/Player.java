d4c098d1bad2076666f5fbe7603a24860ded92bc
NawlFountains <56007803+NawlFountains@users.noreply.github.com>
2021-10-31 19:44:11 -0300

    Cambio implementacion metodo Player

--- src/game/entity/player/Player.java
+++ src/game/entity/player/Player.java
@@ -38,7 +38,8 @@ public class Player extends Entity{
 
 	
 	public void move() {
-		if (attemptMovement != null && x == Math.round(x) && y == Math.round(y)) {			//Chequeamos que se intenta mover a otro lado y que este en el centro de una zona
+		
+		if (attemptMovement != null && isWhole(x) && isWhole(y)) {			//Chequeamos que se intenta mover a otro lado y que este en el centro de una zona
 			switch (attemptMovement) {														//Notese que sabemos que no es igual a movementDirection porque sino seria nula
 			case UP:
 				if (zone.getZoneIn(x, y + 1).getType() == ZoneType.PATH) {
@@ -70,44 +71,44 @@ public class Player extends Entity{
 		case UP:
 			if (zone.getZoneIn(x, y + 1).getType() == ZoneType.PATH) {
 				((GraphicCharacter) graphic).setMovingUp();
-				while (y < y + 1) {						// Aumentamos progresivamente la posiciones hasta llegar a la parte entera
-					y += 0.1f;
-					graphic.update(x,y);				// Actualizamos la grafica
-				}
-				zone = zone.getZoneIn(x,y);				
+				y += 0.1f;
+				graphic.update(x,y);				// Actualizamos la grafica
+				if (isWhole(y)) {					// Si la posicion luego de moverse es entera entonces se encuentra en el centro de una nueva zona.
+					zone = zone.getZoneIn(x, y - 1);
+				}	
 				//TODO medir colision en la nueva zona
 			}
 			break;
 		case RIGHT:
 			if (zone.getZoneIn(x + 1, y).getType() == ZoneType.PATH) {
 				((GraphicCharacter) graphic).setMovingRight();
-				while (y < y + 1) {
-					y += 0.1f;
-					graphic.update(x,y);
-				}
-				zone = zone.getZoneIn(x,y);					
+				x += 0.1f;
+				graphic.update(x,y);
+				if (isWhole(x)) {
+					zone = zone.getZoneIn(x, y - 1);
+				}	
 				//TODO medir colision en la nueva zona
 			}
 			break;
 		case DOWN:
 			if (zone.getZoneIn(x, y - 1).getType() == ZoneType.PATH) {
 				((GraphicCharacter) graphic).setMovingDown();
-				while (y < y + 1) {
-					y += 0.1f;
-					graphic.update(x,y);
-				}
-				zone = zone.getZoneIn(x,y);					
+				y -= 0.1f;
+				graphic.update(x,y);
+				if (isWhole(y)) {
+					zone = zone.getZoneIn(x, y - 1);
+				}			
 				//TODO medir colision en la nueva zona
 			}
 			break;
 		case LEFT:
 			if (zone.getZoneIn(x - 1, y).getType() == ZoneType.PATH) {
 				((GraphicCharacter) graphic).setMovingLeft();
-				while (y < y + 1) {
-					y += 0.1f;
-					graphic.update(x,y);
-				}
-				zone = zone.getZoneIn(x,y);						
+				x -= 0.1f;
+				graphic.update(x,y);
+				if (isWhole(x)) {
+					zone = zone.getZoneIn(x, y - 1);
+				}	
 				//TODO medir colision en la nueva zona
 			}
 			zone = zone.getZoneIn(x,y);						
@@ -116,7 +117,9 @@ public class Player extends Entity{
 		}
 		
 	}
-	
+	private boolean isWhole (float x) {
+		return (x == Math.round(x));
+	}
 	
 	/**
 	 * Evalua si el jugador puede moverse en la direccion pasada, si puede se la asigna
