998116c67a89b40f414b0b1da9c8504baa2eecea
Agustin Ferrante <agus_p3@hotmail.com>
2021-11-01 23:47:45 -0300

    Modificadas clases concretas del paquete prize

--- src/game/entity/prize/ConcreteFruit2.java
+++ src/game/entity/prize/ConcreteFruit2.java
@@ -3,17 +3,15 @@ package game.entity.prize;
 import game.entity.GraphicPrize;
 import game.labyrinth.Zone;
 
-public class ConcreteFruit2 extends Fruit{
+public class ConcreteFruit2 extends Fruit {
+	
 	public ConcreteFruit2(Zone zone) {
-		super();
-		this.zone = zone;
+		super(zone);
 		graphic = new GraphicPrize(zone.getLabyrinth().getImageFactory().getFruit2Image());
-		x = zone.getX();
-		y = zone.getY();
-		zone.addEntity(this);
 	}
 	
 	public void triggerEffect() {
 		//TODO imp
 	}
+	
 }
