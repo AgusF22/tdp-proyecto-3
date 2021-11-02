d862e05e38c88fc5077e48b25a75a16951c8ade2
Agustin Ferrante <agus_p3@hotmail.com>
2021-11-01 23:42:13 -0300

    Modificado prize y subclases

--- src/game/entity/prize/Dot.java
+++ src/game/entity/prize/Dot.java
@@ -4,15 +4,14 @@ import game.entity.GraphicPrize;
 import game.labyrinth.Zone;
 
 public class Dot extends Prize {
+	
 	public Dot(Zone zone) {
-		this.zone = zone;
+		super(zone);
 		graphic = new GraphicPrize(zone.getLabyrinth().getImageFactory().getDotImage());
-		x = zone.getX();
-		y = zone.getY();
-		zone.addEntity(this);
 	}
 	
 	public void triggerEffect() {
 		//TODO imp
 	}
+	
 }
--- src/game/entity/prize/Fruit.java
+++ src/game/entity/prize/Fruit.java
@@ -1,7 +1,11 @@
 package game.entity.prize;
 
+import game.labyrinth.Zone;
+
 public abstract class Fruit extends Prize {
-	public Fruit() {
-		//TODO pasar a abstracto
+	
+	protected Fruit(Zone zone) {
+		super(zone);
 	}
+	
 }
