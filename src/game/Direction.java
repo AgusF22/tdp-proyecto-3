63b0075025b3e88cb4fef0d996f7b039459774c8
Agustin Ferrante <agus_p3@hotmail.com>
2021-11-02 18:14:38 -0300

    Modificado Direction.java

--- src/game/Direction.java
+++ src/game/Direction.java
@@ -1,10 +1,31 @@
 package game;
 
+/**
+ * Modela una direccion.
+ */
 public enum Direction {
 	
-	UP,
-	DOWN,
-	RIGHT,
-	LEFT;
+	UP		("DOWN"	),
+	DOWN	("UP"	),
+	RIGHT	("LEFT"	),
+	LEFT	("RIGHT");
+	
+	private String opposite;
+	
+	/**
+	 * Crea una nueva instancia de direccion.
+	 * @param opposite La direccion opuesta a la nueva direccion.
+	 */
+	private Direction(String opposite) {
+		this.opposite = opposite;
+	}
+	
+	/**
+	 * Retorna la direccion opuesta a esta direccion.
+	 * @return La direccion opuesta a esta direccion.
+	 */
+	public Direction getOpposite() {
+		return Direction.valueOf(opposite);
+	}
 	
 }
