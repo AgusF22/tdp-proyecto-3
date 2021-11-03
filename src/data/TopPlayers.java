f7b7bbc4e9a9068295de66a276bc3b3ac5cae7f1
Agustin Ferrante <agus_p3@hotmail.com>
2021-11-03 18:23:13 -0300

    Modificado diagrama de clases
    cambios en data

--- src/data/TopPlayers.java
+++ src/data/TopPlayers.java
@@ -10,22 +10,22 @@ public class TopPlayers implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	protected List<ScorePlayer> ranking;				// FIXME esto tiene que ser privado o transient -AF
+	protected List<PlayerScore> ranking;				// FIXME esto tiene que ser privado o transient -AF
 	
 	public TopPlayers() {
 		ranking = new ArrayList<>();
 	}
 	
-	public void addPlayer(ScorePlayer p) {
+	public void addPlayer(PlayerScore p) {
 		ranking.add(p);
 	}
 	
-	public List<ScorePlayer> listPlayers() {
+	public List<PlayerScore> listPlayers() {
 		Collections.sort(this.ranking, Collections.reverseOrder());
-		List<ScorePlayer> top = new ArrayList<>();
+		List<PlayerScore> top = new ArrayList<>();
 		int i = 0;
 		
-		for (ScorePlayer p: this.ranking) {
+		for (PlayerScore p: this.ranking) {
 			top.add(p);
 			if (i == 5) break;
 			i++;
